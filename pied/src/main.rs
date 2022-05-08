01c483c8054d5d3358df69928b0043008ea164ee
PeterWrightZhang <zhangzepeng0705@gmail.com>
2022-05-08 22:11:43 +0900

    Update: quit

--- pied/src/main.rs
+++ pied/src/main.rs
@@ -3,6 +3,9 @@ use std::io::{self, Read};
 fn main() {
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
-        println!("{}", c)
+        println!("{}", c);
+        if c == 'q' {
+            break;
+        }
     }
 }
